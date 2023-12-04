e004270def4adbb761c3ea3c4f9f7c7c581202e4
Krzysiek101 <kmyszkowski7@gmail.com>
2023-12-04 22:10:29 +0100

    dodanie obsugi JS do html

--- login_project/login.js
+++ login_project/login.js
@@ -2,17 +2,20 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-// skrypt dla akcji przycisku Submit w formularzu logowania
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value; // zmienna zawierająca nazwe użytkownika
-    const password = loginForm.password.value; // zmienna zawierająca podane hasło
+function dataValidation(){
+    // skrypt dla akcji przycisku Submit w formularzu logowania
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        const username = loginForm.username.value; // zmienna zawierająca nazwe użytkownika
+        const password = loginForm.password.value; // zmienna zawierająca podane hasło
 
-    // skrypt sprawdzający poprawność danych logowania
-    if (username === "samsep" && password === "") {
-        alert("You have successfully logged in."); 
-        location.reload();
-    } else { // wyświetlenie błędu dla nie poprawnych danych logowania
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+        
+        // skrypt sprawdzający poprawność danych logowania
+        if (username === "samsep" && password === "root_passwor") {
+            alert("You have successfully logged in."); 
+            location.reload();
+        } else { // wyświetlenie błędu dla nie poprawnych danych logowania
+            loginErrorMsg.style.opacity = 1;
+        }
+    })
+}
